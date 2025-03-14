093e3c4c1d466056e4aaff699b3520fe228b3f18
Azer147 <188928311+Azer147@users.noreply.github.com>
2025-03-14 01:43:36 +0100

    New feature: spreading outfit module The recipient can choose to fight against it, or let the outfit spread fully (restraints included - Equip/bind progressively the outfit code associated - Can be set remotely (using lscg remote settings) - When the outfit has finished to spread fully, will repeat the process X times (repeat amount and interval configurable) - When finished, all item will remain on the target

---
+++ src/Settings/Models/spreading-outfit.ts
@@ -0,0 +1,34 @@
+import { BaseSettingsModel, ModuleStats } from "./base";
+
+
+export interface SpreadingOutfitModuleStats extends ModuleStats {
+    CurrentOutfitIndex: number;
+    CurrentRepeatNumber: number;
+    NextActivationTime: number; // in ms
+    ActivatedBy: number; // only needed as a spell ?
+}
+
+export interface SpreadingOutfitSettingsModel extends SpreadingOutfitPublicSettingsModel {
+    AllowedRemote: boolean;
+    AllowSelfStop: boolean;
+    Internal: SpreadingOutfitModuleStats;
+}
+
+export interface SpreadingOutfitPublicSettingsModel extends BaseSettingsModel {
+    Active: boolean;
+    Locked: boolean;
+    Lockable: boolean;
+    Outfit1: SpreadingOutfitCodeConfig;
+    Outfit2: SpreadingOutfitCodeConfig;
+    Outfit3: SpreadingOutfitCodeConfig;
+    //DelayActive: boolean;
+    //DelayTime: number;
+    RepeatInterval: number; // in min
+    ItemInterval: number; // in sec
+    RepeatNumber: number;
+}
+
+export interface SpreadingOutfitCodeConfig {
+    Code: string;
+    Enabled: boolean;
+}
